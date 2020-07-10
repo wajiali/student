be95660b190a3cb6cbcc2867c869b6384ed98209
wajiali <67791143+wajiali@users.noreply.github.com>
2020-07-10 04:41:16 -0700

    Add files via upload

---
+++ 1question.php
@@ -0,0 +1,23 @@
+
+<html>
+    
+    <body>
+    
+      
+    <form action="runner2.php" method="get"/>
+Enter  Number: <input name="num1"/><br><br>
+
+    <input type="submit" name="submit" value="submit"/>
+    
+    
+    
+    
+    
+    </body>
+    
+    
+    
+    
+    
+    
+</html>
